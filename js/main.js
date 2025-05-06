80582b0b0651cf1d6cd2b80508269e404ae52ff5
lesitoan <lesitoan.media@gmail.com>
2025-05-06 12:04:09 +0700

    update home ui and create pagination feature

--- js/main.js
+++ js/main.js
@@ -1,19 +1,72 @@
+$.getJSON("data/data.json", function (data) {
+  let foods = data?.foods || [];
+  data = foods;
+  let dataCurrPage = null;
 
-Handlebars.registerHelper("minTypePrice", function(types) {
-    if (!Array.isArray(types) || types.length === 0) return "";
-    return Math.min(...types.map(t => t.value));
+  let paginationTemplate = null;
+
+  $.get("../componets/pagination.html", function (html) {
+    paginationTemplate = Handlebars.compile(html);
+    renderHomePage(1);
   });
 
+  function renderHomePage(page) {
+    dataCurrPage = getDataPerPage(page, foods);
+    loadHtml(".main-content", "../componets/cardList.html", dataCurrPage.data);
+    renderPagination();
+  }
 
-$.getJSON( "data/data.json", function( data ) {
+  function renderPagination() {
+    if (!paginationTemplate) return;
+    const rs = paginationTemplate({
+      page: dataCurrPage.page,
+      totalPage: dataCurrPage.totalPage,
+    });
+    $(".pagination-content").html(rs);
 
-    $.get("../componets/card.html", function (html) {
-        var tmpl = Handlebars.compile(html);
-
-        var rs = tmpl(data);
-        $(".content").html(rs);
+    $(".prev-page")
+      .off("click")
+      .on("click", () => {
+        if (dataCurrPage.page > 1) {
+          renderHomePage(dataCurrPage.page - 1);
+        }
       });
 
+    $(".next-page")
+      .off("click")
+      .on("click", () => {
+        if (dataCurrPage.page < dataCurrPage.totalPage) {
+          renderHomePage(dataCurrPage.page + 1);
+        }
+      });
+  }
+});
 
+Handlebars.registerHelper("minTypePrice", function (types) {
+  if (!Array.isArray(types) || types.length === 0) return "";
+  return Math.min(...types.map((t) => t.value));
+});
 
+const getDataPerPage = (page, data) => {
+  const pageSize = 8;
+  if (!data || data.length === 0) return {};
+  if (page < 1) return {};
+
+  const totalPage = Math.ceil(data.length / pageSize);
+  const start = (page - 1) * pageSize;
+  const end = Math.min(page * pageSize, data.length);
+
+  return {
+    page: page,
+    totalPage: totalPage,
+    data: data.slice(start, end),
+  };
+};
+
+const loadHtml = (parent, filePath, data) => {
+  $.get(filePath, function (html) {
+    var tmpl = Handlebars.compile(html);
+    var rs = tmpl(data);
+    $(parent).html(rs);
   });
+};
